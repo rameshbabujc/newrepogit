74262db4881d4d32b5f05230193ac9bdad075a5c
edurekaganesh <ganesh@edureka.co>
2020-02-09 22:02:00 +0530

    firstcommit

---
+++ ganesh.java
@@ -0,0 +1,9 @@
+class ganesh{
+
+	public static void main(String[] args){
+		System.out.println("This is our initial program");
+		System.out.println("this is from firstbranch");
+                System.out.println("changes have been mad");
+}
+
+}
